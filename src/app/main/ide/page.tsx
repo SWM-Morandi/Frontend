9f2b134f049314fb6965abb1f46e488fbcc18657
yeobi01 <yeobi_01@naver.com>
2023-07-09 22:44:21 +0900

    FIX: 모의테스트 환경 문제 정보 컴포넌트로 빼기

--- src/app/main/ide/page.tsx
+++ src/app/main/ide/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import Editor from '@monaco-editor/react';
 import Axios from 'axios';
 
+import ProblemInfo from '../../../components/main/ide/problemInfo';
+
 export default function IDE() {
   const [userCode, setUserCode] = useState('');
   const [userLang, setUserLang] = useState('cpp');
@@ -43,32 +45,7 @@ export default function IDE() {
       <div className="flex flex-row pl-[15px] pr-[15px]">
         <div className="flex flex-col w-[40vw] h-[90vh] border-2 border-black rounded-2xl mr-[10px]">
           <div className="p-[20px]">
-            <div className="text-[30px]">문제 이름</div>
-            <div className="mt-[20px]" />
-            <div className="text-[20px]">문제</div>
-            <div className="mt-[10px]" />
-            <div>
-              재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27,
-              ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다. 크기 3의
-              패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩
-              있는 패턴이다.
-            </div>
-            <div>
-              N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의
-              (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 예를
-              들어 크기 27의 패턴은 예제 출력 1과 같다.
-            </div>
-            <div className="mt-[20px]" />
-            <div className="text-[20px]">입력</div>
-            <div className="mt-[10px]" />
-            <div>
-              첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에
-              대해 N=3k이며, 이때 1 ≤ k ≤ 8이다.
-            </div>
-            <div className="mt-[20px]" />
-            <div className="text-[20px]">출력</div>
-            <div className="mt-[10px]" />
-            <div>첫째 줄부터 N번째 줄까지 별을 출력한다.</div>
+            <ProblemInfo />
           </div>
         </div>
         <div className="flex flex-col w-[60vw]">
