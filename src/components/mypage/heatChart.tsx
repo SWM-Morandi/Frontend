6d86faa496a85bd59729ddc2a337f672fadb5a7b
yeobi01 <yeobi_01@naver.com>
2023-07-10 16:04:25 +0900

    FIX: 테두리 만들기

--- src/components/mypage/heatChart.tsx
+++ src/components/mypage/heatChart.tsx
@@ -5,17 +5,19 @@ import 'react-calendar-heatmap/dist/styles.css';
 
 export default function HeatChart() {
   return (
-    <div className="w-[1100px]">
-      <CalendarHeatmap
-        startDate={new Date('2015-04-02')}
-        endDate={new Date('2016-04-01')}
-        values={[
-          { date: '2016-01-01', count: 12 },
-          { date: '2016-01-22', count: 122 },
-          { date: '2016-01-30', count: 38 },
-          // ...and so on
-        ]}
-      />
+    <div className="flex flex-row justify-center w-[1100px] border border-gray-400 rounded-xl shadow-xl">
+      <div className="w-[1000px] py-8">
+        <CalendarHeatmap
+          startDate={new Date('2015-04-02')}
+          endDate={new Date('2016-04-01')}
+          values={[
+            { date: '2016-01-01', count: 12 },
+            { date: '2016-01-22', count: 122 },
+            { date: '2016-01-30', count: 38 },
+            // ...and so on
+          ]}
+        />
+      </div>
     </div>
   );
 }
