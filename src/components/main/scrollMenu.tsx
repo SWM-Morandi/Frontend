f7a3344bf93695e77aaba80f7f59d81c02021074
yeobi01 <yeobi_01@naver.com>
2023-07-10 16:04:39 +0900

    FIX: 주석 지우기

--- src/components/main/scrollMenu.tsx
+++ src/components/main/scrollMenu.tsx
@@ -4,111 +4,11 @@ import React from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import 'react-horizontal-scrolling-menu/dist/styles.css';
 import './scroll.css';
+import Link from 'next/link';
 
 import CodingTestCard from './codingTestCard';
 import AlgorithmCard from './algorithmCard';
 import MainCard from './mainCard';
-
-const getItems = () =>
-  Array(20)
-    .fill(0)
-    .map((_, ind) => ({ id: `element-${ind}` }));
-
-export function CodingTestCardScroll() {
-  // 기존에 있던 코드
-  // const [items, setItems] = React.useState(getItems);
-  // const [selected, setSelected] = React.useState([]);
-  // const [position, setPosition] = React.useState(0);
-
-  // const isItemSelected = (id: string) => !!selected.find((el) => el === id);
-
-  // const handleClick =
-  //   (id: any) =>
-  //   ({ getItemById, scrollToItem }: any) => {
-  //     const itemSelected = isItemSelected(id);
-
-  //     setSelected((currentSelected) =>
-  //       itemSelected
-  //         ? currentSelected.filter((el) => el !== id)
-  //         : currentSelected.concat(id),
-  //     );
-  //   };
-
-  return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-      <CodingTestCard />
-    </ScrollMenu>
-  );
-}
-
-export function AlgorithmCardScroll() {
-  return (
-    <>
-      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-      </ScrollMenu>
-    </>
-  );
-}
-
-export function MainScroll() {
-  return (
-    <>
-      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-      </ScrollMenu>
-    </>
-  );
-}
 
 function LeftArrow() {
   const { isFirstItemVisible, scrollPrev } =
@@ -139,17 +39,6 @@ function LeftArrow() {
   );
 }
 
-// function RightArrow() {
-//   const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
-
-//   return (
-//     <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-//       Right
-//     </Arrow>
-
-//   );
-// }
-
 function RightArrow() {
   const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
 
@@ -178,94 +67,60 @@ function RightArrow() {
   );
 }
 
-// 기본적으로 들어가있던 카드
-function Card({ onClick, selected, title, itemId }: any) {
-  const visibility = React.useContext(VisibilityContext);
-
+export function MainScroll() {
   return (
-    <div
-      onClick={() => onClick(visibility)}
-      style={{
-        width: '160px',
-        border: '1px solid black',
-        margin: '4px',
-      }}
-      tabIndex={0}
-    >
-      <div className="card">
-        <div>{title}</div>
-        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
-        <div>selected: {JSON.stringify(!!selected)}</div>
-      </div>
-      <div
-        style={{
-          height: '200px',
-        }}
-      />
-    </div>
+    <>
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+        {Array(10)
+          .fill(0)
+          .map((_, idx) => {
+            return (
+              <Link href="/main/coding-test/before" key={idx}>
+                <MainCard />
+              </Link>
+            );
+          })}
+      </ScrollMenu>
+    </>
   );
 }
 
-// function Arrow({
-//   children,
-//   disabled,
-//   onClick,
-//   className,
-// }: {
-//   children: React.ReactNode;
-//   disabled: boolean;
-//   onClick: VoidFunction;
-//   className?: String;
-// }) {
-//   return (
-//     <button
-//       disabled={disabled}
-//       onClick={onClick}
-//       className={'arrow' + `-${className}`}
-//       style={{
-//         cursor: 'pointer',
-//         display: 'flex',
-//         flexDirection: 'column',
-//         justifyContent: 'center',
-//         right: '1%',
-//         opacity: disabled ? '0' : '1',
-//         userSelect: 'none',
-//       }}
-//     >
-//       {children}
-//     </button>
-//   );
-// }
+export function CodingTestCardScroll() {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {Array(10)
+        .fill(0)
+        .map((_, idx) => {
+          return (
+            <Link href="/main/coding-test/before" key={idx}>
+              <CodingTestCard
+                componey={`네이버${idx}`}
+                problemCount={4}
+                time={2}
+                startLevel="S3"
+                endLevel="G3"
+              />
+            </Link>
+          );
+        })}
+    </ScrollMenu>
+  );
+}
 
-// function Arrow({
-//   children,
-//   disabled,
-//   onClick,
-//   className,
-// }: {
-//   children: React.ReactNode;
-//   disabled: boolean;
-//   onClick: VoidFunction;
-//   className?: String;
-// }) {
-//   return (
-//     <button
-//       disabled={disabled}
-//       onClick={onClick}
-//       className="opacity-0 hover:opacity-100"
-//       style={{
-//         cursor: 'pointer',
-//         display: 'flex',
-//         flexDirection: 'column',
-//         justifyContent: 'center',
-//         // opacity: disabled ? '0' : '1',
-//         userSelect: 'none',
-//         position: 'absolute',
-//         height: '28%',
-//         zIndex: '999',
-//       }}
-//     >
-//       {children}
-//     </button>
-//   );
-// }
+export function AlgorithmCardScroll() {
+  return (
+    <>
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+        {Array(10)
+          .fill(0)
+          .map((_, idx) => {
+            return (
+              <Link href="/main/coding-test/before" key={idx}>
+                <AlgorithmCard />
+              </Link>
+            );
+          })}
+      </ScrollMenu>
+    </>
+  );
+}
