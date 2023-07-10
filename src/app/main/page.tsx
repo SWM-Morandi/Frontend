a0f3c7894f1b3e984b479cd49b6563cc6c579505
yeobi01 <yeobi_01@naver.com>
2023-07-10 09:53:27 +0900

    FIX: 메인페이지 스크롤 적용

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,60 +3,50 @@ import React from 'react';
 import Header from '../../components/main/mainHeader';
 import Content from '../../components/main/mainContent';
 import Footer from '../../components/main/mainFooter';
-import Card from '../../components/main/mainCard';
+import Card from '../../components/main/codingTestCard';
 import Link from 'next/link';
 
-import Test from '../../components/main/scrollMenu';
+import {
+  CodingTestCardScroll,
+  AlgorithmCardScroll,
+  MainScroll,
+} from '../../components/main/scrollMenu';
 
 export default function Main() {
   return (
     <main>
       <Header></Header>
 
-      <Test />
-
-      <Content>
-        <div>최근에 푼 문제</div>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div className="flex flex-col pl-[50px] pr-[50px]">
+        <div className="text-[30px]">최근에 푼 문제</div>
+        <div className="flex flex-row ">
           <Link href="/main/coding-test/before">
             <Card />
           </Link>
           <Card />
         </div>
+      </div>
 
-        <div>사용자에게 추천하는 코딩테스트</div>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+      <div className="mt-[30px]" />
 
-        <div>기업별 코딩테스트</div>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+      <div className="pl-[50px] pr-[50px]">
+        <div className="text-[30px]">사용자에게 추천하는 코딩테스트</div>
+        <MainScroll />
+      </div>
 
-        <div>알고리즘 분류별 공부하기</div>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-      </Content>
+      <div className="mt-[30px]" />
+
+      <div className="pl-[50px] pr-[50px]">
+        <div className="text-[30px]">기업별 코딩테스트</div>
+        <CodingTestCardScroll />
+      </div>
+
+      <div className="mt-[30px]" />
+
+      <div className="pl-[50px] pr-[50px]">
+        <div className="text-[30px]">알고리즘 분류별 공부하기</div>
+        <AlgorithmCardScroll />
+      </div>
       <Footer />
     </main>
   );
--- src/components/main/scrollMenu.tsx
+++ src/components/main/scrollMenu.tsx
@@ -3,43 +3,110 @@
 import React from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import 'react-horizontal-scrolling-menu/dist/styles.css';
+import './scroll.css';
+
+import CodingTestCard from './codingTestCard';
+import AlgorithmCard from './algorithmCard';
+import MainCard from './mainCard';
 
 const getItems = () =>
   Array(20)
     .fill(0)
     .map((_, ind) => ({ id: `element-${ind}` }));
 
-function App() {
-  const [items, setItems] = React.useState(getItems);
-  const [selected, setSelected] = React.useState([]);
-  const [position, setPosition] = React.useState(0);
+export function CodingTestCardScroll() {
+  // 기존에 있던 코드
+  // const [items, setItems] = React.useState(getItems);
+  // const [selected, setSelected] = React.useState([]);
+  // const [position, setPosition] = React.useState(0);
 
-  const isItemSelected = (id: string) => !!selected.find((el) => el === id);
+  // const isItemSelected = (id: string) => !!selected.find((el) => el === id);
 
-  const handleClick =
-    (id: any) =>
-    ({ getItemById, scrollToItem }: any) => {
-      const itemSelected = isItemSelected(id);
+  // const handleClick =
+  //   (id: any) =>
+  //   ({ getItemById, scrollToItem }: any) => {
+  //     const itemSelected = isItemSelected(id);
 
-      setSelected((currentSelected) =>
-        itemSelected
-          ? currentSelected.filter((el) => el !== id)
-          : currentSelected.concat(id),
-      );
-    };
+  //     setSelected((currentSelected) =>
+  //       itemSelected
+  //         ? currentSelected.filter((el) => el !== id)
+  //         : currentSelected.concat(id),
+  //     );
+  //   };
 
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {items.map(({ id }) => (
-        <Card
-          itemId={id} // NOTE: itemId is required for track items
-          title={id}
-          key={id}
-          onClick={handleClick(id)}
-          selected={isItemSelected(id)}
-        />
-      ))}
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
+      <CodingTestCard />
     </ScrollMenu>
+  );
+}
+
+export function AlgorithmCardScroll() {
+  return (
+    <>
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+        <AlgorithmCard />
+      </ScrollMenu>
+    </>
+  );
+}
+
+export function MainScroll() {
+  return (
+    <>
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+      </ScrollMenu>
+    </>
   );
 }
 
@@ -48,22 +115,70 @@ function LeftArrow() {
     React.useContext(VisibilityContext);
 
   return (
-    <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-      Left
-    </Arrow>
+    <button
+      disabled={isFirstItemVisible} // 필요 없는거 같음
+      onClick={() => scrollPrev()}
+      className="opacity-0 hover:opacity-30"
+      style={{
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        userSelect: 'none',
+        position: 'absolute',
+        left: '50px',
+        height: '320px',
+        width: '50px',
+        zIndex: '999',
+        backgroundColor: 'gray',
+      }}
+    >
+      ←
+    </button>
   );
 }
+
+// function RightArrow() {
+//   const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+
+//   return (
+//     <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+//       Right
+//     </Arrow>
+
+//   );
+// }
 
 function RightArrow() {
   const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
 
   return (
-    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-      Right
-    </Arrow>
+    <button
+      disabled={isLastItemVisible}
+      onClick={() => scrollNext()}
+      className="opacity-0 hover:opacity-30"
+      style={{
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        userSelect: 'none',
+        position: 'absolute',
+        right: '50px',
+        height: '320px',
+        width: '50px',
+        zIndex: '999',
+        backgroundColor: 'gray',
+      }}
+    >
+      →
+    </button>
   );
 }
 
+// 기본적으로 들어가있던 카드
 function Card({ onClick, selected, title, itemId }: any) {
   const visibility = React.useContext(VisibilityContext);
 
@@ -91,35 +206,66 @@ function Card({ onClick, selected, title, itemId }: any) {
   );
 }
 
-function Arrow({
-  children,
-  disabled,
-  onClick,
-  className,
-}: {
-  children: React.ReactNode;
-  disabled: boolean;
-  onClick: VoidFunction;
-  className?: String;
-}) {
-  return (
-    <button
-      disabled={disabled}
-      onClick={onClick}
-      className={'arrow' + `-${className}`}
-      style={{
-        cursor: 'pointer',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        right: '1%',
-        opacity: disabled ? '0' : '1',
-        userSelect: 'none',
-      }}
-    >
-      {children}
-    </button>
-  );
-}
+// function Arrow({
+//   children,
+//   disabled,
+//   onClick,
+//   className,
+// }: {
+//   children: React.ReactNode;
+//   disabled: boolean;
+//   onClick: VoidFunction;
+//   className?: String;
+// }) {
+//   return (
+//     <button
+//       disabled={disabled}
+//       onClick={onClick}
+//       className={'arrow' + `-${className}`}
+//       style={{
+//         cursor: 'pointer',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         right: '1%',
+//         opacity: disabled ? '0' : '1',
+//         userSelect: 'none',
+//       }}
+//     >
+//       {children}
+//     </button>
+//   );
+// }
 
-export default App;
+// function Arrow({
+//   children,
+//   disabled,
+//   onClick,
+//   className,
+// }: {
+//   children: React.ReactNode;
+//   disabled: boolean;
+//   onClick: VoidFunction;
+//   className?: String;
+// }) {
+//   return (
+//     <button
+//       disabled={disabled}
+//       onClick={onClick}
+//       className="opacity-0 hover:opacity-100"
+//       style={{
+//         cursor: 'pointer',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         // opacity: disabled ? '0' : '1',
+//         userSelect: 'none',
+//         position: 'absolute',
+//         height: '28%',
+//         zIndex: '999',
+//       }}
+//     >
+//       {children}
+//     </button>
+//   );
+// }
