d9148f50bc81cfe0381854334379f1d985b982f8
yeobi01 <yeobi_01@naver.com>
2023-07-10 09:54:09 +0900

    ADD: 네이버 아이콘 assets 추가

---
+++ src/assets/icons/naver.tsx
@@ -0,0 +1,14 @@
+export default function Naver() {
+  return (
+    <svg
+      role="img"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Naver</title>
+      <path d="M16.273 12.845 7.376 0H0v24h7.726V11.156L16.624 24H24V0h-7.727v12.845Z" />
+    </svg>
+  );
+}
