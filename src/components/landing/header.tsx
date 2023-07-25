aa4dc6744c313bd3c2b1c251db1249bf3175a3f2
yeobi01 <yeobi_01@naver.com>
2023-07-26 03:44:46 +0900

    [FIX] 랜딩페이지 헤더 수정

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -3,27 +3,19 @@ import Link from 'next/link';
 export default function LandingHeader() {
   return (
     <>
-      <header className="text-gray-600 body-font">
+      <header className="text-white body-font bg-black">
         <div className=" mx-[3rem] flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
-            href="/dashboard"
-            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            href="/"
+            className="flex title-font font-medium items-center mb-4 md:mb-0"
           >
             <span className="ml-3 text-[2rem] font-black">모랜디</span>
           </Link>
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <Link href="/dashboard" className="mr-5 hover:text-gray-900">
-              혼랜디
-            </Link>
-            <Link href="/dashboard" className="mr-5 hover:text-gray-900">
-              모랜디
-            </Link>
-          </nav>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/auth/signin" className="mr-5 hover:text-gray-900">
+            <Link href="/auth/signin" className="mr-5 hover:text-[#FFA935]">
               로그인
             </Link>
-            <Link href="/auth/signup" className="mr-5 hover:text-gray-900">
+            <Link href="/auth/signup" className="mr-5 hover:text-[#FFA935]">
               회원가입
             </Link>
           </nav>
