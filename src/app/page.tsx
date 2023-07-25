87a5758392b627ebbd170f24211cf0a83f04b5b9
yeobi01 <yeobi_01@naver.com>
2023-07-26 03:40:31 +0900

    [FEAT] 랜딩페이지 개발

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import Header from '../components/landing/header';
 import Footer from '../components/landing/footer';
+import LandingIcon from './landingIcon';
 
-export default function Home() {
+export default function Page() {
   return (
     <main>
       <Header />
-      <div className="mb-[20vh]"></div>
-      <div className="text-center">랜딩페이지</div>
-      <div className="mb-[20vh]"></div>
+      <div className="flex flex-col justify-center items-center text-white bg-black py-[4rem]">
+        <div className="text-[3.5rem] font-black mb-[2rem]">
+          IT업계를 위한 최고의 솔루션
+        </div>
+        <div className="mb-[5rem]">
+          <div>모랜디는 가장 실전의 유사한 경험을 제공하여</div>
+          <div>IT 업계 취업에 가장 필수적인 경험을 제공합니다.</div>
+        </div>
+        <LandingIcon />
+      </div>
       <Footer />
     </main>
   );
