d85c4698f2b1489ad89fd8e1d43e0098135993a1
yeobi01 <yeobi_01@naver.com>
2023-07-26 01:53:55 +0900

    [ADD] 로그인 / 회원가입 페이지 OAuth Provider 아이콘 추가

---
+++ src/assets/icons/googleIcon.tsx
@@ -0,0 +1,275 @@
+export default function GoogleIcon() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <mask
+          id="mask0_3_83"
+          maskUnits="userSpaceOnUse"
+          x="8"
+          y="7"
+          width="10"
+          height="9"
+        >
+          <mask
+            id="mask1_3_83"
+            maskUnits="userSpaceOnUse"
+            x="8"
+            y="7"
+            width="10"
+            height="9"
+          >
+            <mask
+              id="mask2_3_83"
+              maskUnits="userSpaceOnUse"
+              x="8"
+              y="7"
+              width="10"
+              height="9"
+            >
+              <mask
+                id="mask3_3_83"
+                maskUnits="userSpaceOnUse"
+                x="-13"
+                y="-13"
+                width="44"
+                height="44"
+              >
+                <path
+                  d="M30.7476 -12.745H-12.7524V30.755H30.7476V-12.745Z"
+                  fill="white"
+                />
+              </mask>
+              <g mask="url(#mask3_3_83)">
+                <path
+                  d="M17.1684 9.19399C17.1684 8.58877 17.1116 8.0119 17.0171 7.45396H8.99792V10.7449H13.5749C13.3763 11.804 12.7805 12.7118 11.8727 13.317V15.4542H14.6246C16.2322 13.9695 17.1589 11.7945 17.1589 9.20345L17.1684 9.19399Z"
+                  fill="white"
+                />
+              </g>
+            </mask>
+            <g mask="url(#mask2_3_83)">
+              <path
+                d="M-13.6973 -13.6925H31.694V31.6988H-13.6973V-13.6925Z"
+                fill="white"
+              />
+            </g>
+          </mask>
+          <g mask="url(#mask1_3_83)">
+            <path
+              d="M565.988 -718.204H-12.7511V30.7523H565.988V-718.204Z"
+              fill="white"
+            />
+          </g>
+        </mask>
+        <g mask="url(#mask0_3_83)">
+          <path
+            d="M21.898 2.72276H4.271V20.1795H21.898V2.72276Z"
+            fill="#3E82F1"
+          />
+        </g>
+        <mask
+          id="mask4_3_83"
+          maskUnits="userSpaceOnUse"
+          x="1"
+          y="10"
+          width="14"
+          height="8"
+        >
+          <mask
+            id="mask5_3_83"
+            maskUnits="userSpaceOnUse"
+            x="1"
+            y="10"
+            width="14"
+            height="8"
+          >
+            <mask
+              id="mask6_3_83"
+              maskUnits="userSpaceOnUse"
+              x="1"
+              y="10"
+              width="14"
+              height="8"
+            >
+              <mask
+                id="mask7_3_83"
+                maskUnits="userSpaceOnUse"
+                x="-13"
+                y="-13"
+                width="44"
+                height="44"
+              >
+                <path
+                  d="M30.7476 -12.745H-12.7524V30.755H30.7476V-12.745Z"
+                  fill="white"
+                />
+              </mask>
+              <g mask="url(#mask7_3_83)">
+                <path
+                  d="M8.99794 17.5156C11.2959 17.5156 13.225 16.7496 14.634 15.4541L11.8822 13.3169C11.1162 13.8275 10.1422 14.1301 8.99794 14.1301C6.78511 14.1301 4.90326 12.6361 4.23185 10.6218H1.38544V12.8251C2.785 15.6054 5.65978 17.5156 8.98848 17.5156H8.99794Z"
+                  fill="white"
+                />
+              </g>
+            </mask>
+            <g mask="url(#mask6_3_83)">
+              <path
+                d="M-13.6973 -13.6925H31.694V31.6988H-13.6973V-13.6925Z"
+                fill="white"
+              />
+            </g>
+          </mask>
+          <g mask="url(#mask5_3_83)">
+            <path
+              d="M565.988 -718.204H-12.7511V30.7523H565.988V-718.204Z"
+              fill="white"
+            />
+          </g>
+        </mask>
+        <g mask="url(#mask4_3_83)">
+          <path
+            d="M19.3637 5.88867H-3.33191V22.239H19.3637V5.88867Z"
+            fill="#32A753"
+          />
+        </g>
+        <mask
+          id="mask8_3_83"
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="5"
+          width="5"
+          height="8"
+        >
+          <mask
+            id="mask9_3_83"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="5"
+            width="5"
+            height="8"
+          >
+            <mask
+              id="mask10_3_83"
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="5"
+              width="5"
+              height="8"
+            >
+              <mask
+                id="mask11_3_83"
+                maskUnits="userSpaceOnUse"
+                x="-13"
+                y="-13"
+                width="44"
+                height="44"
+              >
+                <path
+                  d="M30.7476 -12.745H-12.7524V30.755H30.7476V-12.745Z"
+                  fill="white"
+                />
+              </mask>
+              <g mask="url(#mask11_3_83)">
+                <path
+                  d="M4.23159 10.6203C4.06138 10.1096 3.96681 9.56112 3.96681 9.00318C3.96681 8.44525 4.06138 7.89675 4.23159 7.3861V5.18277H1.38518C0.808334 6.32701 0.477356 7.63199 0.477356 9.00318C0.477356 10.3744 0.808334 11.6794 1.38518 12.8236L4.23159 10.6203Z"
+                  fill="white"
+                />
+              </g>
+            </mask>
+            <g mask="url(#mask10_3_83)">
+              <path
+                d="M-13.6973 -13.6925H31.694V31.6988H-13.6973V-13.6925Z"
+                fill="white"
+              />
+            </g>
+          </mask>
+          <g mask="url(#mask9_3_83)">
+            <path
+              d="M565.988 -718.204H-12.7511V30.7523H565.988V-718.204Z"
+              fill="white"
+            />
+          </g>
+        </mask>
+        <g mask="url(#mask8_3_83)">
+          <path
+            d="M8.96192 0.45266H-4.23938V17.55H8.96192V0.45266Z"
+            fill="#F9BB00"
+          />
+        </g>
+        <mask
+          id="mask12_3_83"
+          maskUnits="userSpaceOnUse"
+          x="1"
+          y="0"
+          width="14"
+          height="8"
+        >
+          <mask
+            id="mask13_3_83"
+            maskUnits="userSpaceOnUse"
+            x="1"
+            y="0"
+            width="14"
+            height="8"
+          >
+            <mask
+              id="mask14_3_83"
+              maskUnits="userSpaceOnUse"
+              x="1"
+              y="0"
+              width="14"
+              height="8"
+            >
+              <mask
+                id="mask15_3_83"
+                maskUnits="userSpaceOnUse"
+                x="-13"
+                y="-13"
+                width="44"
+                height="44"
+              >
+                <path
+                  d="M30.7476 -12.745H-12.7524V30.755H30.7476V-12.745Z"
+                  fill="white"
+                />
+              </mask>
+              <g mask="url(#mask15_3_83)">
+                <path
+                  d="M8.99776 3.87742C10.246 3.87742 11.3713 4.30294 12.2508 5.15403L14.6906 2.71423C13.2154 1.34304 11.2862 0.501438 8.99776 0.501438C5.66906 0.501438 2.79428 2.41162 1.39471 5.19184L4.24113 7.39522C4.91254 5.38098 6.78493 3.88689 9.00721 3.88689L8.99776 3.87742Z"
+                  fill="white"
+                />
+              </g>
+            </mask>
+            <g mask="url(#mask14_3_83)">
+              <path
+                d="M-13.6973 -13.6925H31.694V31.6988H-13.6973V-13.6925Z"
+                fill="white"
+              />
+            </g>
+          </mask>
+          <g mask="url(#mask13_3_83)">
+            <path
+              d="M565.988 -718.204H-12.7511V30.7523H565.988V-718.204Z"
+              fill="white"
+            />
+          </g>
+        </mask>
+        <g mask="url(#mask12_3_83)">
+          <path
+            d="M19.421 -4.23779H-3.33136V12.1125H19.421V-4.23779Z"
+            fill="#E74235"
+          />
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_3_83">
+          <rect width="18" height="18" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
---
+++ src/assets/icons/naverIcon.tsx
@@ -0,0 +1,16 @@
+export default function NaverIcon() {
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.49917 7.49L4.30413 0H0V14H4.5124V6.51L9.69587 14H14V0H9.49917V7.49Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
