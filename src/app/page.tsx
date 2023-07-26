713de8b0abd82f490931309641d059c89bb5eb17
yeobi01 <yeobi_01@naver.com>
2023-07-26 12:28:55 +0900

    [FIX] 랜딩페이지 글자 가운데 정렬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         <div className="text-[3.5rem] font-black mb-[2rem]">
           IT업계를 위한 최고의 솔루션
         </div>
-        <div className="mb-[5rem]">
+        <div className="flex flex-col items-center mb-[5rem]">
           <div>모랜디는 가장 실전의 유사한 경험을 제공하여</div>
           <div>IT 업계 취업에 가장 필수적인 경험을 제공합니다.</div>
         </div>
