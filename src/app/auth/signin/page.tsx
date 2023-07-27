0eff8f1d89ca66518cc7f62f8eca41f972d40bfd
yeobi01 <yeobi_01@naver.com>
2023-07-27 16:33:46 +0900

    [FEAT] 회원가입 로그인 화면 컴포넌트 분리

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
-import Footer from '../../../components/landing/footer';
-import GoogleIcon from '@/assets/icons/googleIcon';
-import NaverIcon from '@/assets/icons/naverIcon';
-import GithubIcon from '@/assets/icons/githubIcon';
+import Footer from '@/components/landing/footer';
+import SignInBox from '@/components/auth/signInBox';
 
 export default function SingIn() {
   return (
@@ -11,24 +9,7 @@ export default function SingIn() {
         <div className="text-[5rem] font-black">모랜디</div>
         <div className="text-[1.5rem] font-bold">모두의 랜덤 디펜스</div>
         <div className="my-[2rem]">
-          <Link href="/dashboard">
-            <div className="flex flex-row justify-center items-center h-[4rem] w-[27rem] bg-[#F7F7FA] rounded-2xl text-black text-[1.2rem] mb-[1rem]">
-              <GoogleIcon />
-              <div className="ml-[0.5rem]">구글로 로그인하기</div>
-            </div>
-          </Link>
-          <Link href="/dashboard">
-            <div className="flex flex-row justify-center items-center h-[4rem] w-[27rem] bg-[#03C75A] rounded-2xl text-white text-[1.2rem] mb-[1rem]">
-              <NaverIcon />
-              <div className="ml-[0.5rem]">네이버로 로그인하기</div>
-            </div>
-          </Link>
-          <Link href="/dashboard">
-            <div className="flex flex-row justify-center items-center h-[4rem] w-[27rem] bg-gray-900 rounded-2xl text-white text-[1.2rem]">
-              <GithubIcon />
-              <div className="ml-[0.5rem]">깃허브로 로그인하기</div>
-            </div>
-          </Link>
+          <SignInBox />
         </div>
         <div className="mb-[2rem] text-gray-500">
           아직 모랜디 회원이 아니신가요?
---
+++ src/components/auth/signInBox.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import GoogleIcon from '@/assets/icons/googleIcon';
+import NaverIcon from '@/assets/icons/naverIcon';
+import GithubIcon from '@/assets/icons/githubIcon';
+
+export default function SingInBox() {
+  return (
+    <>
+      <Link href="/dashboard">
+        <div className="flex flex-row justify-center items-center h-[4rem] w-[27rem] bg-[#F7F7FA] rounded-2xl text-black text-[1.2rem] mb-[1rem]">
+          <GoogleIcon />
+          <div className="ml-[0.5rem]">구글로 로그인하기</div>
+        </div>
+      </Link>
+      <Link href="/dashboard">
+        <div className="flex flex-row justify-center items-center h-[4rem] w-[27rem] bg-[#03C75A] rounded-2xl text-white text-[1.2rem] mb-[1rem]">
+          <NaverIcon />
+          <div className="ml-[0.5rem]">네이버로 로그인하기</div>
+        </div>
+      </Link>
+      <Link href="/dashboard">
+        <div className="flex flex-row justify-center items-center h-[4rem] w-[27rem] bg-gray-900 rounded-2xl text-white text-[1.2rem]">
+          <GithubIcon />
+          <div className="ml-[0.5rem]">깃허브로 로그인하기</div>
+        </div>
+      </Link>
+    </>
+  );
+}
