06867eaeee5dc7e0a4f3c988ae67cc4a37f5eb4e
yeobi01 <yeobi_01@naver.com>
2023-09-18 16:11:13 +0900

    [FIX] hotfix.. 중..

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
       });
 
     if (!flag) {
-      return NextResponse.rewrite(new URL('/dashboard/boj', request.url));
+      return NextResponse.redirect(new URL('/dashboard/boj', request.url));
     }
   }
 
