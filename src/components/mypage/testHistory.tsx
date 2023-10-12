2058353bdac2fe56eb6803392dd26a62dcfc23ef
yeobi01 <yeobi_01@naver.com>
2023-10-12 15:17:17 +0900

    [FEAT] 시험 히스토리 상세보기 디자인 제외 / 기능 추가

--- src/components/mypage/testHistory.tsx
+++ src/components/mypage/testHistory.tsx
@@ -1,65 +1,10 @@
 'use client';
 
-function ArrowIcon() {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="24" height="24" rx="12" fill="#191F28" />
-      <path
-        d="M12.5456 6.99805L17.561 12.0106L12.6071 16.9617"
-        stroke="white"
-        strokeWidth="1.12821"
-        strokeLinecap="square"
-      />
-      <path
-        d="M16.8995 11.98H6.43793"
-        stroke="white"
-        strokeWidth="1.12821"
-        strokeLinecap="square"
-      />
-    </svg>
-  );
-}
-
 import Gap from '@/utils/gap';
 import { axiosInstance } from '@/api/axiosSetting';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
-
-function HistoryComp({
-  company,
-  date,
-  solvedCount,
-  problemCount,
-}: {
-  company: string | undefined;
-  date: string | undefined;
-  solvedCount: number | undefined;
-  problemCount: number | undefined;
-}) {
-  return (
-    <>
-      <div className="flex flex-row justify-between items-center w-[30rem] px-[3rem] py-[1.5rem] bg-gray-100 rounded-xl">
-        <div className="flex flex-col">
-          <div className="text-[1.5rem] font-bold">{company}</div>
-          <div className="text-gray-500">{date}</div>
-        </div>
-        <div className="flex flex-row items-center">
-          <div className="flex flex-col items-center mr-[1.5rem] bg-white px-[1rem] py-[0.5rem] rounded-xl">
-            <div className="text-[1.3rem] font-bold">{solvedCount}문제</div>
-            <div className="text-gray-500">총 {problemCount}문제</div>
-          </div>
-          <ArrowIcon />
-        </div>
-      </div>
-    </>
-  );
-}
+import TestHistoryDetailModal from './testHistoryDetailModal';
 
 interface AttemptProblemDtoType {
   testProblemId: number;
@@ -122,19 +67,85 @@ export default function TestHistory() {
         {/* 테스트 기록 정보 */}
         <div className="grid grid-cols-2 gap-10">
           {Array.isArray(data) ? (
-            data.map((item) => (
-              <HistoryComp
-                company={item.testTypename}
-                date={item.testDate}
-                solvedCount={item.acceptCount}
-                problemCount={item.problemCount}
-              />
-            ))
+            data.map((item) => {
+              console.log(item);
+              return <HistoryComponent testHistoryInfo={item} />;
+            })
           ) : (
             <div>에러 발생</div>
           )}
         </div>
       </div>
     </>
+  );
+}
+
+function HistoryComponent({
+  testHistoryInfo,
+}: {
+  testHistoryInfo: TestHistoryInfoType | undefined;
+}) {
+  const [testHistoryDetailModalFlag, setTestHistoryDetailModalFlag] =
+    useState<boolean>(false);
+  return (
+    <>
+      {testHistoryDetailModalFlag ? (
+        <TestHistoryDetailModal
+          setTestHistoryDetailModalFlag={setTestHistoryDetailModalFlag}
+          testHistoryInfo={testHistoryInfo}
+        />
+      ) : null}
+      <div className="flex flex-row justify-between items-center w-[30rem] px-[3rem] py-[1.5rem] bg-gray-100 rounded-xl">
+        <div className="flex flex-col">
+          <div className="text-[1.5rem] font-bold">
+            {testHistoryInfo?.testTypename}
+          </div>
+          <div className="text-gray-500">{testHistoryInfo?.testDate}</div>
+        </div>
+        <div className="flex flex-row items-center">
+          <div className="flex flex-col items-center mr-[1.5rem] bg-white px-[1rem] py-[0.5rem] rounded-xl">
+            <div className="text-[1.3rem] font-bold">
+              {testHistoryInfo?.acceptCount}문제
+            </div>
+            <div className="text-gray-500">
+              총 {testHistoryInfo?.problemCount}문제
+            </div>
+          </div>
+          <ArrowIcon
+            onClick={() => {
+              setTestHistoryDetailModalFlag(true);
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function ArrowIcon({ onClick }: { onClick: () => void }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="cursor-pointer"
+      onClick={onClick}
+    >
+      <rect width="24" height="24" rx="12" fill="#191F28" />
+      <path
+        d="M12.5456 6.99805L17.561 12.0106L12.6071 16.9617"
+        stroke="white"
+        strokeWidth="1.12821"
+        strokeLinecap="square"
+      />
+      <path
+        d="M16.8995 11.98H6.43793"
+        stroke="white"
+        strokeWidth="1.12821"
+        strokeLinecap="square"
+      />
+    </svg>
   );
 }
