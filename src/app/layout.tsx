15f16d0f6cdbebbb8e6c3be39390fc9098df2bf8
yeobi01 <yeobi_01@naver.com>
2023-10-16 16:58:38 +0900

    [FEAT] SEO 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import QueryCientLayout from './queryClient';
+import SEO from '@/app/seo';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SEO />
       <body className={inter.className}>
         <QueryCientLayout>{children}</QueryCientLayout>
         <script
---
+++ src/app/seo.tsx
@@ -0,0 +1,25 @@
+const SEO = () => {
+  return (
+    <head>
+      <title>모랜디</title>
+      <meta
+        name="description"
+        content="모두의 랜덤 디펜스: 코딩테스트를 준비하는 최고의 솔루션!"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="모랜디" />
+      <meta
+        property="og:description"
+        content="모두의 랜덤 디펜스: 코딩테스트를 준비하는 최고의 솔루션!"
+      />
+      <meta property="og:url" content="https://morandi.co.kr" />
+      <meta property="og:locale" content="ko_KR" />
+      {/* <meta
+        property="og:image"
+        content="https://wpdkr.cdn.toastoven.net/static/brand/images/main_logo_low.png"
+      /> */}
+    </head>
+  );
+};
+
+export default SEO;
